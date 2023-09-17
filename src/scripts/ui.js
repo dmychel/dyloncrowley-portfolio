577b77d2f77cdfb2825a9284784b49f96b9c6673
dmychel <dmychel@tutanota.com>
2023-09-17 12:04:06 -0400

    create about
    creatAbout function
    	adds innerhtml to about section
    
    Change to createDOM()
    	- moved the element creation outside the function
    	- intialize createAbout function

--- src/scripts/ui.js
+++ src/scripts/ui.js
@@ -1,11 +1,12 @@
+
+// create elements
+const aboutSection = document.createElement('section');
+const workSection = document.createElement('section');
+const stackSection = document.createElement('section');
+const contactSection = document.createElement('section');
+
 export function createDOM() {
     const main = document.querySelector('main')
-
-    // create elements
-    const aboutSection = document.createElement('section');
-    const workSection = document.createElement('section');
-    const stackSection = document.createElement('section');
-    const contactSection = document.createElement('section');
 
     // assign class and ids
     aboutSection.classList = 'about'
@@ -21,5 +22,47 @@ export function createDOM() {
     main.appendChild(workSection);
     main.appendChild(stackSection);
     main.appendChild(contactSection);
+
+    createAbout()
 }
 
+function createAbout() {
+    aboutSection.innerHTML = `
+    <header class="header">
+    <div class="name">
+        <span>Dylon Crowley</span>
+    </div>
+    <div class="nav">
+        <nav class="nav-header" id="header">
+            <ul>
+                <li><a href="">Dev-View</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#work">Work</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+<div class="container">
+    <div class="hero-container">
+        <span class="hero" id="hero"></span>
+    </div>
+    <div class="arrow-container fadeIn-animation" id="arrowContainer">
+        <a href="#work"><img src="img/down.png" alt="Down Arrow"></a>
+        <a href="#work"><img src="img/down.png" alt="Down Arrow"></a>
+        <a href="#work"><img src="img/down.png" alt="Down Arrow"></a>
+    </div>
+</div>`
+}
+
+export function createWork(project) {
+
+}
+
+function createStack() {
+
+}
+
+function createContact() {
+
+}
