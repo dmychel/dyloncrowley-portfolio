e0194fb69fb00a41a7987aa230e98c687a1d51b1
dmychel <dmychel@tutanota.com>
2023-09-17 14:31:23 -0400

    import all images used for dom

---
+++ src/scripts/img.js
@@ -0,0 +1,37 @@
+import cssPNG from '../img/skill/css-3.png'
+import gitPNG from '../img/skill/git.png'
+import htmlPNG from '../img/skill/html-5.png'
+import jsPNG from '../img/skill/js.png'
+import linuxPNG from '../img/skill/linux-platform.png'
+import webpackPNG from '../img/skill/webpack.png'
+import calculator from '../img/thumbnail/calculator-thumbmail.png'
+import gameArchive from '../img/thumbnail/game-archive.jpg'
+import streetFighterCombat from '../img/thumbnail/street-combat-thumbnail.png'
+import terminal from '../img/thumbnail/terminal-thumbnail.jpg'
+import weather from '../img/thumbnail/weather-app-thumbnail.jpg'
+
+export const cssIMG = new Image()
+export const htmlIMG = new Image()
+export const jsIMG = new Image()
+export const webpackIMG = new Image()
+export const gitIMG = new Image()
+export const linuxIMG = new Image()
+
+export const calculatorIMG = new Image()
+export const gameArchiveIMG = new Image()
+export const streetFighterCombatIMG = new Image()
+export const terminalIMG = new Image()
+export const weatherIMG = new Image()
+
+cssIMG.src = cssPNG
+htmlIMG.src = htmlPNG
+jsIMG.src = jsPNG
+webpackIMG.src = webpackPNG
+gitIMG.src = gitPNG
+linuxIMG.src = linuxPNG
+
+calculatorIMG.src = calculator
+gameArchiveIMG.src = gameArchive
+streetFighterCombatIMG.src = streetFighterCombat
+terminalIMG.src = terminal
+weatherIMG.src = weather
