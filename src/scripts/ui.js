f2df029bfadef1a1619d02d07c68a99a1fdcb1bf
dmychel <dmychel@tutanota.com>
2023-09-17 12:46:28 -0400

    create stack + objects
    - created stack section innerhtml
    - added project objects to creatwork function

--- src/scripts/ui.js
+++ src/scripts/ui.js
@@ -1,4 +1,4 @@
-
+import { renderProject } from "./dynamicUI";
 // create elements
 const aboutSection = document.createElement('section');
 const workSection = document.createElement('section');
@@ -24,6 +24,8 @@ export function createDOM() {
     main.appendChild(contactSection);
 
     createAbout()
+    createWork()
+    createStack()
 }
 
 function createAbout() {
@@ -55,12 +57,37 @@ function createAbout() {
 </div>`
 }
 
-export function createWork(project) {
+export function createWork() {
+    const weatherAPP = {
+        title: 'Weather App',
+        image: 'heres a link',
+        link: 'https://dmychel.github.io/weather-app/',
+        description: 'This is a simple weather app that uses a API from weatherapi. This app includes the ability to search cities and change the weather information from fahrenheit to celsius. The app has a couple of other features worth reading about in the README on the github repo.'
+    };
 
+    const videGameArchive = {
+        title: 'Video Game Archive',
+        image: 'link',
+        link: 'https://dmychel.github.io/dyloncrowley-portfolio/pages/terminal.html',
+        description: 'Introducing a user-friendly archive that captures essential details about your games. You can add any game from any generation,console, and genre. You even have the ability to add a picture for coverart.'
+    };
+
+    const projects = [weatherAPP, videGameArchive]
+
+    renderProject(projects)
 }
 
 function createStack() {
-
+    stackSection.innerHTML = `
+        <h2>Stack</h2>
+        <div class="stack-img">
+        <img src="#" alt="#">
+            <img src="" alt="#">
+            <img src="" alt="#">
+            <img src="" alt="#">
+            <img src="" alt="#">
+            <img src="" alt="#">
+        </div>`
 }
 
 function createContact() {
