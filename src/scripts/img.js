efffb7a3fef1899b158757efea0bcafd4839940c
dmychel <dmychel@tutanota.com>
2023-09-26 16:10:14 -0400

    removed stack image

--- src/scripts/img.js
+++ src/scripts/img.js
@@ -1,22 +1,9 @@
-import cssPNG from '../img/skill/css-3.png'
-import gitPNG from '../img/skill/git.png'
-import htmlPNG from '../img/skill/html-5.png'
-import jsPNG from '../img/skill/js.png'
-import linuxPNG from '../img/skill/linux-platform.png'
-import webpackPNG from '../img/skill/webpack.png'
 import calculator from '../img/thumbnail/calculator-thumbmail.png'
 import gameArchive from '../img/thumbnail/game-archive.jpg'
 import streetFighterCombat from '../img/thumbnail/street-combat-thumbnail.png'
 import terminal from '../img/thumbnail/terminal-thumbnail.jpg'
 import weather from '../img/thumbnail/weather-app-thumbnail.jpg'
 import hero from '../img/hero.JPG'
-
-export const cssIMG = new Image()
-export const htmlIMG = new Image()
-export const jsIMG = new Image()
-export const webpackIMG = new Image()
-export const gitIMG = new Image()
-export const linuxIMG = new Image()
 
 export const calculatorIMG = new Image()
 export const gameArchiveIMG = new Image()
@@ -26,13 +13,6 @@ export const weatherIMG = new Image()
 
 export const heroIMG = new Image()
 export const backgroundIMG = new Image()
-
-cssIMG.src = cssPNG
-htmlIMG.src = htmlPNG
-jsIMG.src = jsPNG
-webpackIMG.src = webpackPNG
-gitIMG.src = gitPNG
-linuxIMG.src = linuxPNG
 
 calculatorIMG.src = calculator
 gameArchiveIMG.src = gameArchive
